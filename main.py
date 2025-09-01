2a9a909d92ee2305a8614a8be486c4b5a3a71796
DBouraoui <contact@dbouraoui.fr>
2025-09-01 10:13:01 +0200

    minor: update endpoint

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import models
+import schemas
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/url", response_model=schemas.Url)
+def create_url(user: schemas.UrlCreate, db: Session = Depends(get_db)):
+    db_url = models.Url(url=user.url, name=user.name)
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    return db_url
 
+@app.get("/urls/", response_model=list[schemas.Url])
+def read_url( db: Session = Depends(get_db)):
+    users = db.query(models.Url).all()
+    return users
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+from fastapi import HTTPException
 
+@app.delete("/urls/{id}", response_model=schemas.Url)
+def delete_url(id: int, db: Session = Depends(get_db)):
+    url = db.query(models.Url).filter(models.Url.id == id).first()
+    if not url:
+        raise HTTPException(status_code=404, detail="URL not found")
+    db.delete(url)
+    db.commit()
+    return url
+
+@app.put("/urls/{id}", response_model=schemas.Url)
+def update_url(url: schemas.UrlCreate ,id: int, db: Session = Depends(get_db)):
+    url_entity = db.query(models.Url).filter(models.Url.id == id).first()
+
+    if not url_entity:
+        raise HTTPException(status_code=404, detail="URL not found")
+
+    if url.url != url_entity.url:
+        url_entity.url = url.url
+
+    if url.name != url_entity.name:
+        url_entity.name = url.name
+
+    db.commit()
+    db.refresh(url_entity)
+
+    return url_entity
